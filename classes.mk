879fa7e77f90ae38e0e081b3de7be2d863875560
David A. Solin <david@joval.org>
2012-01-31 11:36:46 -0600

    first commit

---
+++ classes.mk
@@ -0,0 +1,18 @@
+CLASSES=\
+	org.apache.commons.net.DatagramSocketClient	\
+	org.apache.commons.net.DatagramSocketFactory	\
+	org.apache.commons.net.DefaultDatagramSocketFactory	\
+	org.apache.commons.net.io.FromNetASCIIInputStream	\
+	org.apache.commons.net.io.FromNetASCIIOutputStream	\
+	org.apache.commons.net.io.ToNetASCIIInputStream	\
+	org.apache.commons.net.io.ToNetASCIIOutputStream	\
+	org.apache.commons.net.tftp.TFTP	\
+	org.apache.commons.net.tftp.TFTPAckPacket	\
+	org.apache.commons.net.tftp.TFTPClient	\
+	org.apache.commons.net.tftp.TFTPDataPacket	\
+	org.apache.commons.net.tftp.TFTPErrorPacket	\
+	org.apache.commons.net.tftp.TFTPPacket	\
+	org.apache.commons.net.tftp.TFTPPacketException	\
+	org.apache.commons.net.tftp.TFTPReadRequestPacket	\
+	org.apache.commons.net.tftp.TFTPRequestPacket	\
+	org.apache.commons.net.tftp.TFTPWriteRequestPacket
